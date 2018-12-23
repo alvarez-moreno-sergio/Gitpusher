a60713c74c13ad3a41eb6f2519bebd3a8575edbf
Sergio Alvarez Moreno <nosth_49@hotmail.com>
2018-12-23 19:37:14 +0100

    Add files via upload

---
+++ AUTOGIT.bat
@@ -0,0 +1,9 @@
+echo       GITPUSHER
+echo ==============================
+cd C:\Users\therealnosth\Desktop\Advent-of-Code-2017\
+git add .
+git commit -m "Wake up! A brand new Day is comming!"
+git push -u origin master
+echo ==============================
+echo       GITPUSHER
+pause
